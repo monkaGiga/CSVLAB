e8e41c8ddfc68f111d4da62117ced8008d1b22e8
BT_1N3_02 <byu1510@bths.edu>
2018-12-14 10:14:14 -0500

    a

---
+++ src/sample/App.java
@@ -0,0 +1,34 @@
+package sample;
+public class App {
+    private String status;
+    private int complete = 0;
+
+
+    public App(String status, String dd, String de, String mc) {
+        this.status = status;
+
+        if(dd.equalsIgnoreCase("Complete")){
+            complete++;
+        }
+        if(de.equalsIgnoreCase("Complete")){
+            complete++;
+        }
+        if(mc.equalsIgnoreCase("Complete")){
+            complete++;
+        }
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public int getComplete(){
+        return complete;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + status + " ]" + " Completed Procedures: " + complete;
+    }
+
+}
--- src/sample/CVSReader.java
+++ src/sample/CVSReader.java
@@ -13,31 +13,17 @@ public class CVSReader {
     public static List<App> readAppsFromCSV(String fileName) {
         List<App> Apps = new ArrayList<>();
         Path pathToFile = Paths.get(fileName);
-
-        // create an instance of BufferedReader
-        // using try with resource, Java 7 feature to close resources
         try (BufferedReader br = Files.newBufferedReader(pathToFile,
                 StandardCharsets.US_ASCII)) {
-
-            // read the first line from the text file
             String line = br.readLine();
             line = br.readLine();
 
-            // loop until all lines are read
-            while (line != null) {
 
-                // use string.split to load a string array with the values from
-                // each line of
-                // the file, using a comma as the delimiter
+            while (line != null)
+            {
                 String[] attributes = line.split(",");
-
                 App App = createApp(attributes);
-
-                // adding backup.App into ArrayList
                 Apps.add(App);
-
-                // read next line before looping
-                // if end of file reached, line would be null
                 line = br.readLine();
             }
 
@@ -53,7 +39,6 @@ public class CVSReader {
         String dd = metadata[7];
         String de = metadata[8];
         String mc = metadata[9];
-        // create and return backup.App of this metadata
         return new App(status, dd, de, mc);
     }
 }
